1ef9f6df5ad1a9597747ee9ee70c741a499b01c4
kolorowyAleksander <kolorowyaleksander@users.noreply.github.com>
2019-02-08 16:38:39 +0100

    Checkpoint

--- database/scripts/database.sh
+++ database/scripts/database.sh
@@ -3,11 +3,11 @@
 if [[ $1 = "run" ]]
 then
     echo "Creating database"
-    docker exec -it database_cassandra-one_1 "bash /cassandra/database-schema.sh"
+    docker exec -it database_cassandra-one_1 /bin/bash /cassandra/database-schema.sh
 elif [[ $1 = "truncate" ]]
 then
     echo "Truncating database"
-    docker exec -it database_cassanra-one_1 "bash /cassandra/truncate-database.sh"
+    docker exec -it database_cassandra-one_1 bash /cassandra/truncate-database.sh
 else
     echo "Cannot know what to do with: $@"
 fi
